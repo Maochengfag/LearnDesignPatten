9027d245dbb8c127c1038b124bda6c4f37d92bd7
Oliver_Mao <maochengfang@126.com>
2019-06-02 08:56:36 +0800

    Initial Commit

---
+++ LearnDesignPatten/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LearnDesignPatten
+//
+//  Created by Mac on 19/6/2.
+//  Copyright © 2019年 Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LearnDesignPatten/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LearnDesignPatten
+//
+//  Created by Mac on 19/6/2.
+//  Copyright © 2019年 Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
